f0628a79471cc37c6584a517c25f3bb4d4844170
Gwilherm <1017720+gwilherm@users.noreply.github.com>
2024-01-19 10:32:38 +0000

    feat: Refactor item component HTML buttons and add onAssign function to item component

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -7,4 +7,12 @@ import { Component, Input } from '@angular/core';
 })
 export class ItemComponent {
   @Input() character: any = {};
+
+  onAssign(side: string) {
+    if (this.character.side === side) {
+      this.character.side = '';
+    } else {
+      this.character.side = side;
+    }
+  }
 }
