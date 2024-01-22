db3a714d9cad0f147aa28cff1911b6383592eb6b
Gwilherm <1017720+gwilherm@users.noreply.github.com>
2024-01-22 13:07:28 +0000

    Extracting Route Paramters

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,10 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StarWarsService } from '../star-wars.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
-export class ListComponent {
-  @Input() characters: any[] = [];
+export class ListComponent implements OnInit {
+  characters: any[] = [];
+
+  activatedRoute: ActivatedRoute;
+  swService: StarWarsService;
+
+  constructor(activatedRoute: ActivatedRoute, swService: StarWarsService) {
+    this.activatedRoute = activatedRoute;
+    this.swService = swService;
+  }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe((params) => {
+      this.characters = this.swService.getCharacters(params['side']);
+    });
+  }
 }
