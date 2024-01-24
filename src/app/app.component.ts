a57e78a57fc0bd6b0d3b733dfe89a3420579cbb7
Gwilherm <1017720+gwilherm@users.noreply.github.com>
2024-01-24 15:33:27 +0000

    Fixing a "Bug" we Introduced into Our Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StarWarsService } from './star-wars.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CodeSandbox';
+
+  constructor(private swService: StarWarsService) {}
+
+  ngOnInit(): void {
+    this.swService.fetchCharacters();
+  }
 }
